76db9462287d6b7a2a27075f09e1cc1e65cfbab5
Clarice Christine <142429120+clarisv@users.noreply.github.com>
2024-10-10 23:32:56 -0300

    ProvaFafifo.java

---
+++ ProvaFafifo.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.questionario;
+
+public class ProvaFafifo extends ProvaUniversidade {
+        public ProvaFafifo(double notaAV1, double notaAV2){
+        super(notaAV1, notaAV2);
+    }
+     public boolean aprovado(){
+        return calcularMedia() >= 6;
+    }
+}
